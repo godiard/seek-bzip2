f65d59526af9ac0401cc60704345babe00831239
Gonzalo Odiard <godiard@gmail.com>
2015-06-20 17:10:19 -0300

    Initial debian packaging files created by dh_make
    Run dh_make -f ../seek-bzip2-1.0.tar.gz
    as documented here:
    http://www.debian.org/doc/manuals/maint-guide/first.es.html#dh-make

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(seek-bzip2):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="seek-bzip2" command="/usr/bin/seek-bzip2"
---
+++ debian/seek-bzip2.default.ex
@@ -0,0 +1,10 @@
+# Defaults for seek-bzip2 initscript
+# sourced by /etc/init.d/seek-bzip2
+# installed at /etc/default/seek-bzip2 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
