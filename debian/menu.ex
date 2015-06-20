1e635b4108e074b7cd424d5063945ca9f5c10423
Gonzalo Odiard <godiard@gmail.com>
2015-06-20 17:50:01 -0300

    Removed many unneeded debian packaging files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(seek-bzip2):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="seek-bzip2" command="/usr/bin/seek-bzip2"
--- debian/seek-bzip2.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the seek-bzip2 package
-#
-0 4	* * *	root	[ -x /usr/bin/seek-bzip2_maintenance ] && /usr/bin/seek-bzip2_maintenance
